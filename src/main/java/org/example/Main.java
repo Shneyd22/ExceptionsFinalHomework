76e3aad5e4da3447e1ce1f8543b87e740fbe727d
Sergei Schneidmiller <5797221@gmail.com>
2023-12-18 20:36:39 +0300

    Homework is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,92 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] personData = scanner.nextLine().split(" ");
+        System.out.println(Arrays.toString(personData));
+        Person person = new Person(new String[3], null, 0, ' ');
+        dataInputService(personData, person);
+        System.out.println(Arrays.toString(person.getName()));
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        String strDate = formatter.format(person.birthDate);
+        System.out.println(strDate);
+        System.out.println(person.phone);
+        System.out.println(person.gender);
+        fileService(person);
+
+
+    }
+
+    public static void dataInputService(String[] personData, Person person) {
+        int count = 0;
+        if (personData.length == 6) {
+            for (int i = 0; i < personData.length; i++) {
+                if (personData[i].contains(".")) {
+                    try {
+                        person.birthDate = new SimpleDateFormat("dd.MM.yyyy").parse(personData[i]);
+                    } catch (ParseException e) {
+                        throw new RuntimeException("Некорректный ввод даты");
+                    }
+                } else if (personData[i].length() == 1) {
+                    if (personData[i].equals("m") || personData[i].equals("f")) {
+                        person.gender = personData[i].charAt(0);
+                    } else throw new RuntimeException("Введите свой пол в формате m или f!");
+                } else {
+                    try {
+                        person.phone = Long.parseLong(personData[i]);
+                    } catch (NumberFormatException e) {
+                        //throw new RuntimeException("Некорректный ввод номера телефона");
+                        while (count < 3) {
+                            person.name[count] = personData[i + count];
+                            count++;
+                        }
+                    }
+                }
+            }
+        } else throw new RuntimeException("Введено меньше и больше данных, чем требуется");
+    }
+
+    public static void fileService(Person person) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        String strDate = formatter.format(person.birthDate);
+        try {
+            File file = new File(person.name[0] + ".txt");
+            if (file.createNewFile()) {
+                FileWriter fileWriter = new FileWriter(person.name[0] + ".txt", false);
+                fileWriter.write(
+                        "<" + person.name[0] + ">" +
+                                "<" + person.name[1] + ">" +
+                                "<" + person.name[2] + ">" +
+                                "<" + strDate + ">" +
+                                "<" + person.phone + ">" +
+                                "<" + person.gender + ">");
+                fileWriter.close();
+            } else {
+                FileWriter fileWriter = new FileWriter(person.name[0] + ".txt", true);
+//                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//                bufferedWriter.append(
+                fileWriter.write("\n" +
+                        "<" + person.name[0] + ">" +
+                                "<" + person.name[1] + ">" +
+                                "<" + person.name[2] + ">" +
+                                "<" + strDate + ">" +
+                                "<" + person.phone + ">" +
+                                "<" + person.gender + ">");
+                fileWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при создании файла");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,57 +1,53 @@
 package org.example;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class PersonData {
+public class Person {
     public String[] name = new String[3];
     public Date birthDate;
-    public int phone;
+    public long phone;
     public char gender;
 
-    public PersonData(String[] name, Date birthDate, int phone, char gender) {
+    public Person(String[] name, Date birthDate, int phone, char gender) {
         this.name = name;
         this.birthDate = birthDate;
         this.phone = phone;
         this.gender = gender;
     }
-    public void inputPersonData (String[] name, Date birthDate, int phone, char gender) {
-        Scanner n1 = new Scanner(System.in);
-        System.out.println("Введите фамилию: ");
-        if (n1 != null) {
-            name[0] = n1.nextLine();
-        } else throw new RuntimeException("Вы не ввели фамилию!");
-        Scanner n2 = new Scanner(System.in);
-        System.out.println("Введите имя: ");
-        if (n2 != null) {
-            name[1] = n2.nextLine();
-        } else throw new RuntimeException("Вы не ввели имя!");
-        Scanner n3 = new Scanner(System.in);
-        System.out.println("Введите отчество: ");
-        if (n3 != null) {
-            name[2] = n3.nextLine();
-        } else throw new RuntimeException("Вы не ввели отчество!");
-        Scanner bd = new Scanner(System.in);
-        System.out.println("Введите дату рождения в формате dd.mm.yyyy: ");
-        try {
-            birthDate = new SimpleDateFormat("dd.MM.yyyy").parse(bd.nextLine());
-        } catch (ParseException e) {
-            throw new RuntimeException("Некорректный ввод даты");
-        }
-        Scanner p = new Scanner(System.in);
-        System.out.println("Введите номер телефона без пробелов и тире: ");
-        try {
-            phone = Integer.parseInt(p.nextLine());
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Некорректный ввод номера телефона");
-        }
-        Scanner g = new Scanner(System.in);
-        System.out.println("Введите свой пол в формате m или f");
-        if (g.nextLine() == "m" || g.nextLine() == "f"){
-            gender = g.nextLine().charAt(0);
-        }else throw new RuntimeException("Введите свой пол в формате m или f!");
+    public String[] getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setName(String[] name) {
+        this.name[0] = name[0];
+        this.name[1] = name[1];
+        this.name[2] = name[2];
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
     }
 }
